32c719f37b37e3c67d2599f2966d6ff07a00ab2f
alvaro <alvarepas@hotmail.com>
2021-11-03 14:37:02 -0500

    inicio

---
+++ src/main/java/com/petalone/petalone/PetAloneApplication.java
@@ -0,0 +1,13 @@
+package com.petalone.petalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetAloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petalone/petalone/PetAloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petalone.petalone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
