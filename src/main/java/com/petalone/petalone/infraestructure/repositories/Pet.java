34b7ae1405b9995762264f68e23923a457a122a7
alvaro <alvarepas@hotmail.com>
2021-11-03 14:56:03 -0500

    lombok en PET

--- src/main/java/com/petalone/petalone/infraestructure/repositories/Pet.java
+++ src/main/java/com/petalone/petalone/infraestructure/repositories/Pet.java
@@ -8,61 +8,19 @@ import java.util.Objects;
 
 @Entity
 public class Pet {
-    private String idPet;
-    private String name;
-    private Short age;
-    private String petType;
 
     @Id
     @Column(name = "id_pet", nullable = false, length = 50)
-    public String getIdPet() {
-        return idPet;
-    }
-
-    public void setIdPet(String idPet) {
-        this.idPet = idPet;
-    }
-
+    private String idPet;
     @Basic
     @Column(name = "name", nullable = true, length = 20)
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    private String name;
     @Basic
     @Column(name = "age", nullable = true)
-    public Short getAge() {
-        return age;
-    }
-
-    public void setAge(Short age) {
-        this.age = age;
-    }
-
+    private Short age;
     @Basic
     @Column(name = "pet_type", nullable = true, length = 20)
-    public String getPetType() {
-        return petType;
-    }
+    private String petType;
 
-    public void setPetType(String petType) {
-        this.petType = petType;
-    }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Pet pet = (Pet) o;
-        return Objects.equals(idPet, pet.idPet) && Objects.equals(name, pet.name) && Objects.equals(age, pet.age) && Objects.equals(petType, pet.petType);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(idPet, name, age, petType);
-    }
 }
